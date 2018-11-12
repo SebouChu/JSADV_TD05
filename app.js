894306f012d074a2a385855fdea5573f250ab6e3
Sébastien Gaya <sebastien.gaya@gmail.com>
2018-11-12 14:52:10 +0100

    refactor

--- app.js
+++ app.js
@@ -17,6 +17,25 @@ Vue.component("movieItem", {
     </li>`
 });
 
+Vue.component("movieForm", {
+    props: ["movie"],
+    template: `<form>
+            <label for="movie.title">Title : </label>
+            <input type="text" v-model="movie.title" placeholder="Interstellar" size="50"><br>
+
+            <label for="movie.year">Year : </label>
+            <input type="number" style="width:5em;" v-model="movie.year" placeholder="2014"><br>
+
+            <label for="movie.director">Director : </label>
+            <input type="text" v-model="movie.director" placeholder="Christopher Nolan"><br>
+
+            <label for="movie.synopsis">Synopsis : </label><br>
+            <textarea v-model="movie.synopsis" rows="8" cols="80" placeholder="Lorem ipsum dolor sit amet..."></textarea><br>
+
+            <button type="button" v-on:click="$emit('submit')">End</button>
+        </form>`
+});
+
 var app = new Vue({
     el: "#app",
     data: {
