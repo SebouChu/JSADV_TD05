4b46c103d51b312fc788b5e05f7b06228519c5c3
Sébastien Gaya <sebastien.gaya@gmail.com>
2018-12-12 21:33:16 +0100

    init step5

---
+++ etape5/src/components/movieitem.vue
@@ -0,0 +1,23 @@
+<template>
+<li>
+  <p v-on:click="toggleDetails = !toggleDetails"><strong>{{ movie.title }} ({{ movie.year }})</strong></p>
+  <div class="movie-details" v-if="toggleDetails">
+    <p><i>Réalisé par {{ movie.director }}</i><br>{{ movie.synopsis }}</p>
+    <p>
+      <button type="button" v-on:click="$emit('edit')">Edit</button>
+      <button type="button" v-on:click="$emit('destroy')" style="color:red;">Destroy</button>
+    </p>
+  </div>
+</li>
+</template>
+
+<script>
+export default {
+  props: ["movie"],
+  data: function () {
+    return {
+      toggleDetails: false
+    }
+  }
+}
+</script>
